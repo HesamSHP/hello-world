d9a8328d2bb4ef2bfbc8b744b313381a9dff8577
hesam <hesamshp@gmail.com>
2020-03-03 09:20:53 +0330

    This are some changes just for learning.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+@app.route('/healthcheck/ok')
+def check():
+	ret = {"message": "EVERYTHING IS OK"}
+	return jsonify(ret), 200
 
 @app.route('/')
 def main_page():
-	return 'Hello'
+	return 'Hello World.'
 
 def process():
-		
+	pass
+
+if __name__ == "__main__":
+	app.run('0.0.0.0',5000, debug=True)
