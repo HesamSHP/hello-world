adde1f2d141921e898ac984112d22caee4983b04
hesamshp <hesamshp@gmail.com>
2020-03-07 14:53:29 +0330

    This is a commit just for test

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def main_page():
-	
-	return 'Hello'
+def main_page():	
+	return 'Hello World'
+
+@app.route('/check')
+def check_app():
+	return 'Everything is OK.'
+
+if __name__== '__main__':
+	app.run('0.0.0.0',5000, True)
+
+
+
+
